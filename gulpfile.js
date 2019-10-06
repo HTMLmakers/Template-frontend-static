f752ea4fff4a87950d75a9f163731183f7d3cd8e
Aleksey Iegorov <aleksey.iegorov@gmail.com>
2019-10-06 11:17:54 +0300

    Добавлены таски для обработки html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
 
-const {src, dest} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
+
+const del = require('del');
+const fileInclude = require('gulp-file-include');
 
 
 const srcRoot = './src';
@@ -21,9 +24,7 @@ const srcPath = {
   js: {
 
   },
-  pages: {
-
-  },
+  pages: `${srcRoot}/pages/`,
   styles: {
 
   }
@@ -42,9 +43,7 @@ const devPath = {
   js: {
 
   },
-  pages: {
-
-  },
+  pages: `${devRoot}/`,
   styles: {
 
   }
@@ -54,3 +53,55 @@ const buildPath = {
 
 };
 
+
+//-----------------------------------------------------
+
+
+
+
+/**
+ * Html
+ * --------------------------------------------------------------------------
+ */
+
+
+/**
+ * Сборка html:
+ * 1. Сборка всех инклудов из ./src/components/, ./src/pages/
+ * 2. Очистка от лишних коментариев, переводов строк !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * 3. Сохранение собраных файлов .html в ./dev/
+ */
+
+function compileHtml() {
+  return src(`${srcPath.pages}/*.html`)
+    //.pipe(plumber())
+    .pipe(fileInclude())
+    //.pipe(replace(/(\<\!\-\-)(?!\s*build\:|\*|\s*endbuild\s)[^>]*(\S*\-\-\>)/gi, ''))
+		//.pipe(replace(/$(\n)(\s|\n|\t)+^/gm, '$1'))
+    .pipe(dest(`${devPath.pages}`));
+}
+
+/**
+ * Очистка каталога ./dev/ от всех файлов .html
+ */
+
+function cleanHtml() {
+  return del([`${devPath.pages}/*.html`]);
+}
+
+/**
+ * Отслеживание html:   чистка каталога ./dev/ от всех файлов .html
+ * 1. Отслеживание изменений в файлах .html ./src/components/, ./src/pages/
+ */
+
+function watchHtml() {
+  watch('./src/pages/*.html', { events: 'unlink'}, series(cleanHtml, compileHtml));
+};
+
+
+//exports.default = function() {
+//  watch('./src/pages/*.html', { events: 'unlink'}, series(cleanHtml, compileHtml));
+//};
+
+
+exports.default = watchHtml;
