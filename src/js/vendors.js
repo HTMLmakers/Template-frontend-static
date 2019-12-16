100786656a22f3bf494bff0d0dcdf62c7ebba126
Andrey <andrey.yegorov.mail@gmail.com>
2019-12-16 14:40:25 +0200

    добавил js uikit для popup

--- src/js/vendors.js
+++ src/js/vendors.js
@@ -1,4 +1,4 @@
 @include('../node_modules/jquery/dist/jquery.min.js')
 @include('../node_modules/jquery-migrate/dist/jquery-migrate.min.js')
 @include('../node_modules/bxslider/dist/jquery.bxslider.min.js')
-@include('../node_modules/fancybox/dist/js/jquery.fancybox.pack.js')
+@include('../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js')
