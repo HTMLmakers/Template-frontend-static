ee96327dc7e7db10850d6185cffa554b14125496
Andrey <andrey.yegorov.mail@gmail.com>
2019-12-17 18:03:24 +0200

    добавил js uikit для slider

--- src/js/ui-kit/slider.js
+++ src/js/ui-kit/slider.js
@@ -1,1 +1,19 @@
+(($) => {
+  const options = {
+    mode: 'fade',
+    slideWidth: 0,
+    controls: false,
+    responsive: true,
+    pagerSelector: '.slider__pagination',
+  };
 
+  const slider = $('.slider__list').bxSlider(options);
+
+  $('.slider__next').on('click', () => {
+    slider.goToNextSlide();
+  });
+
+  $('.slider__prev').on('click', () => {
+    slider.goToPrevSlide();
+  });
+})(jQuery);
