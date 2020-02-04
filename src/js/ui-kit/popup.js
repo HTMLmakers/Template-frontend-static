2e82502e663c4c46f44f44398525b235582cd1b0
Aleksey Iegorov <aleksey.iegorov@gmail.com>
2020-02-04 19:20:58 +0200

    Правки компонента ui-kit/popup

--- src/js/ui-kit/popup.js
+++ src/js/ui-kit/popup.js
@@ -7,14 +7,15 @@
   };
 
   $('[data-uikit-popup-open]').on('click', function (ev) {
-    const popupName = $(this).data('uikit-popup-open');
 
     ev.preventDefault();
 
-    $.fancybox.open($(`[data-uikit-popup=${popupName}]`), options);
-  });
+    const popupName = $(this).data('uikit-popup-open');
 
-  $('.popup__close').on('click', () => {
-    $.fancybox.close();
+    $.fancybox.open($(`[data-uikit-popup=${popupName}]`), options);
+
+    $('.control-close').on('click', () => {
+      $.fancybox.close();
+    });
   });
 })(jQuery);
