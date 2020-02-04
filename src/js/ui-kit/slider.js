fee6a657bfd72ce955723ffd3f2426f3a111f0ab
Aleksey Iegorov <aleksey.iegorov@gmail.com>
2020-02-04 19:35:46 +0200

    Правки компонента ui-kit/slider

--- src/js/ui-kit/slider.js
+++ src/js/ui-kit/slider.js
@@ -1,18 +1,36 @@
 (($) => {
   const options = {
-    mode: 'fade',
-    controls: false,
-    responsive: true,
-    pagerSelector: '.slider__pagination',
+    default: {
+      arrows: false,
+      dots: true
+    },
+    'slider': {
+      ...this.default
+    }
+    //,
+    //'slider-name-1': {
+    //
+    //}
   };
 
-  const slider = $('.slider__list').bxSlider(options);
+  const $sliderContainer = $('[data-uikit-slider-container]');
 
-  $('.slider__next').on('click', () => {
-    slider.goToNextSlide();
-  });
+  $sliderContainer.each(function(index, el) {
+    const $container = $(el);
+    const $slider = $container.find('[data-uikit-slider]');
+    const $buttonPrev = $container.find('.control-arrow--prev');
+    const $buttonNext = $container.find('.control-arrow--next');
+    const $pagination = $container.find('.control-pagination');
 
-  $('.slider__prev').on('click', () => {
-    slider.goToPrevSlide();
+    options.default.appendDots = $pagination;
+    $slider.slick({...options.default, ...options[$slider.data('uikit-slider')]});
+
+    $buttonNext.on('click', function () {
+      $slider.slick('slickNext');
+    });
+    $buttonPrev.on('click', function () {
+      $slider.slick('slickPrev');
+    });
+
   });
 })(jQuery);
