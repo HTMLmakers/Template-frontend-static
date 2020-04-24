825260e7bd36ae9c6f1f3a0cbf8806e8c724e606
Aleksey Iegorov <aleksey.iegorov@gmail.com>
2020-04-24 12:52:18 +0300

    Правки в js/ui-kit

--- src/js/components.js
+++ src/js/components.js
@@ -1,3 +1,3 @@
-@include('components/core/header/header.js')
+@include('components/shared/layout/header/header.js')
 @include('components/features/back-to-top/back-to-top.js')
 @include('components/features/toggle/toggle.js')
