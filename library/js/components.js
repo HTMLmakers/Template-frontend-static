a0a3e827035a55182b4d21781ffde02f66cc0485
andre.y.egorov <andrey.yegorov.mail@gmail.com>
2020-07-13 15:23:09 +0300

    added library folder

---
+++ library/js/components.js
@@ -0,0 +1,4 @@
+@include('./components/shared/layout/header/header.js')
+@include('./components/shared/layout/header-fixed/header-fixed.js')
+@include('./components/shared/common/back-to-top/back-to-top.js')
+@include('./components/shared/common/toggle/toggle.js')
